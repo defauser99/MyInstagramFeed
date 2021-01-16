97bc00ad49e517529d057014c6a7053ad50c6821
defauser99 <defaihsanr@gmail.com>
2021-01-16 09:35:46 +0700

    Initial commit

---
+++ app/src/main/java/binar/ch4/myinstagramfeed/MainActivity.kt
@@ -0,0 +1,11 @@
+package binar.ch4.myinstagramfeed
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
